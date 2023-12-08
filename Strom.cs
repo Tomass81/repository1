e9076e0f466abaaa895719d8d5baabeb8be83d97
Tomass81 <153209652+Tomass81@users.noreply.github.com>
2023-12-08 13:06:07 +0100

    Vytvorenie triedy strom

---
+++ Strom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Strom
+    {
+        public Strom() { }
+        ~Strom() { }
+        public double Volume
+        {
+            get { return V = Math.PI * (r * r) * v;  }
+        }
+
+        protected double r;
+        protected double v;
+        protected double V;
+    }
+    class Baobab : Strom
+    {
+        public double Volume
+        {
+            get { return V = Math.PI * (r * r) * v; }
+        }
+        protected double r;
+        protected double v;
+        protected double V;
+    }
+    class Ihlicnan : Strom 
+    {
+        public double Volume
+        {
+            get { return V = Math.PI * (r * r) * v; }
+        }
+        protected double r;
+        protected double v;
+        protected double V;
+    }
+}
