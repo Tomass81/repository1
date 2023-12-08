15818abbe3e0696dba5d75233cde7ca9a961ea40
Tomass81 <153209652+Tomass81@users.noreply.github.com>
2023-12-08 12:06:24 +0100

    Vytvorenie programu na vypocet objemu stromu

---
+++ Hodina.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double v;
+            double V;
+            
+
+            Console.WriteLine("Zadaj radius: ");
+            r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zadaj vysku: ");
+            v = double.Parse(Console.ReadLine());
+
+            V = ((Math.PI / 3) * (r * r) * v);
+            Console.WriteLine("Objem je: " + V);
+
+            Console.ReadKey();  
+        }
+    }
+}
